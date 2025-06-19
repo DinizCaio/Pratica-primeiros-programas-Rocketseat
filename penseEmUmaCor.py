5e35117e29feb0dcbe44f1c233b4fadcbc39bf6f
Caio Diniz <dinizcaio97@gmail.com>
2025-06-19 14:18:09 -0300

    Exercício 2 finalizado

---
+++ penseEmUmaCor.py
@@ -0,0 +1,15 @@
+import os
+
+print("Pense em uma cor.")
+input("Quando decidir, aperte ENTER.")
+
+os.system("cls")
+
+print("Agore pense em uma ferramenta com essa cor.")
+input("Quando decidir, aperte ENTER.")
+
+os.system("cls")
+
+print("Você pensou em um... Martelo vermelho!")
+print()
+print("Acertei? :D")
