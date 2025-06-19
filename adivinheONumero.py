0386e7878368c6d06e14e5ca82a90af502af2fc6
Caio Diniz <dinizcaio97@gmail.com>
2025-06-19 13:50:48 -0300

    Exercício 1 finalizado

---
+++ adivinheONumero.py
@@ -0,0 +1,12 @@
+import os
+
+print("Escolha um número entre 1 e 10.")
+input("Quando estiver preparado, aperte ENTER.")
+
+os.system("cls")
+
+print("Um segundo...")
+print()
+print("Você pensou no 7...")
+print()
+print("Acertei? xD")
